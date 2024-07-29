726215169745da612bfd2353ab93428647878e26
Ulises Rendón <fdulises@outlook.com>
2024-07-28 20:54:54 -0600

    [update] setting routes for allowing ui

--- Config/routes.php
+++ Config/routes.php
@@ -7,23 +7,23 @@ use App\Product\Controller\StockController;
 use App\Product\Controller\ProductController;
 use App\Product\Controller\ProviderController;
 
-Router::any('/', [DefaultController::class, 'home']);
-Router::post('/migrate', [Migration::class, 'start']);
+Router::any('/api/v1', [DefaultController::class, 'home']);
+Router::post('/api/v1/migrate', [Migration::class, 'start']);
 
-Router::post('/product', [ProductController::class, 'create']);
-Router::get('/product', [ProductController::class, 'list']);
-Router::get('/product/:id', [ProductController::class, 'getById']);
-Router::patch('/product/:id', [ProductController::class, 'update']);
-Router::delete('/product/:id', [ProductController::class, 'deleteById']);
+Router::post('/api/v1/product', [ProductController::class, 'create']);
+Router::get('/api/v1/product', [ProductController::class, 'list']);
+Router::get('/api/v1/product/:id', [ProductController::class, 'getById']);
+Router::patch('/api/v1/product/:id', [ProductController::class, 'update']);
+Router::delete('/api/v1/product/:id', [ProductController::class, 'deleteById']);
 
-Router::get('/provider', [ProviderController::class, 'list']);
-Router::post('/provider', [ProviderController::class, 'create']);
-Router::get('/provider/:id', [ProviderController::class, 'getById']);
-Router::patch('/provider/:id', [ProviderController::class, 'update']);
-Router::delete('/provider/:id', [ProviderController::class, 'delete']);
+Router::get('/api/v1/provider', [ProviderController::class, 'list']);
+Router::post('/api/v1/provider', [ProviderController::class, 'create']);
+Router::get('/api/v1/provider/:id', [ProviderController::class, 'getById']);
+Router::patch('/api/v1/provider/:id', [ProviderController::class, 'update']);
+Router::delete('/api/v1/provider/:id', [ProviderController::class, 'delete']);
 
-Router::get('/stock', [StockController::class, 'list']);
-Router::get('/stock/:id', [StockController::class, 'getByProductId']);
-Router::post('/stock', [StockController::class, 'create']);
-Router::post('/stock/:id/sync', [StockController::class, 'productStockSync']);
-Router::delete('/stock', [StockController::class, 'delete']);
+Router::get('/api/v1/stock', [StockController::class, 'list']);
+Router::get('/api/v1/stock/:id', [StockController::class, 'getByProductId']);
+Router::post('/api/v1/stock', [StockController::class, 'create']);
+Router::post('/api/v1/stock/:id/sync', [StockController::class, 'productStockSync']);
+Router::delete('/api/v1/stock', [StockController::class, 'delete']);
