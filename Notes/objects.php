870208f11b526f998503e4c1f76f58aa6cabe06e
Ulises Rendon <fdulises@outlook.com>
2024-09-17 16:59:34 -0600

    [wip]

--- Notes/objects.php
+++ Notes/objects.php
@@ -20,7 +20,7 @@ Los objetos son estructuras que permiten encapsular y agrupar datos y su comport
 
 <p>La mejor forma y la principal forma de definir objetos es mediante clases.</p>
 
-<p>Las clases son el plano en donde se detallan las características de los objetos.</p>
+<p>Las clases son el plano en donde se detallan las características de los objetos y con solo una clase se pueden generar muchos objetos, por lo que son muy útiles para reutilizar código.</p>
 
 <p>Para crear un objeto a partir de una clase se usa la palabra clave de PHP <strong>new</strong></p>
 
@@ -58,21 +58,35 @@ class Product
 $Product = new Product;
 /*</code></pre>
 
-<p>Ahora tenemos una clase producto que agrupa y oculta los datos, simplemente definimos una variable para cada dato requerido y declaramos el tipo de dato, y antes de cada variable usamos la palabra <strong>private</strong> que es el indicativo de que el dato debe estar oculto.</p>
+<p>Ahora tenemos una clase producto que agrupa y oculta sus datos, simplemente definimos una variable para cada dato requerido y declaramos el tipo de dato, y antes de cada variable usamos la palabra <strong>private</strong> que es el indicativo de que el dato debe estar oculto y asi evitar que pueda ser accedido desde el exterior del objeto.</p>
 
-<p>Ciertamente podríamos haber definido un arreglo asociativo con estos datos.</p>
-
-<p>Pero ¿como evitas que los datos sean modificados por tus algoritmos o por otros módulos?, ó ¿como evitas que si los valores que van a ser modificados no se corrompan?</p>
+<p>Podríamos haber definido un arreglo asociativo con estos datos, pero ¿como evitas que los datos sean modificados por tus algoritmos o por otros módulos?, ó ¿como evitas que los valores que van a ser modificados no se corrompan?</p>
 
 <p>En el caso de un producto de una plataforma de comercio, el titulo y la descripción son valores que no suelen cambiar durante la ejecución del programa.</p>
 
 <p>Por otro lado el precio es un valor que podría variar si se aplican impuestos o descuentos pero tanto el precio como las existencias son valores que de ser modificados tendrían que seguir algún criterio, como el de nunca poder ser valores negativos.</p>
 
-<p>Para resolver estas cuestiones crearemos las funciones que definirán el comportamiento interno del producto.</p>
+<p>Para resolver estas cuestiones crearemos las funciones que definirán el comportamiento interno del producto, pero antes necesitamos aclarar ciertos detalles técnicos.</p>
 
-<p>Vamos a modificar el código anterior y a añadir mecanismos para poder leer los datos del producto y al final del código vamos a imprimirlos para comprobar que realmente estamos accediendo a ellos.</p>
+<h2>Propiedades y métodos y su visibilidad</h2>
 
-<p></p>
+<p>Para poder declarar el comportamiento de un objeto se usa el formato que ya vimos antes para declarar funciones, solo que ahora estas funciones estarán dentro de las llaves que delimitan la clase.</p>
+
+<p>Y para permitir que estas funciones de clase puedan ser accedidas desde fuera del objeto las declaremos anteponiendoles la palabra reservada <strong>public</strong></p>
+
+<p>Los datos encapsulados en objetos se conocen como <strong>propiedades</strong> y las funciones encapsuladas en objectos se conocen como <strong>métodos</strong> y cuando para referirse a cualquiera de las dos cosas o a ambas en general se les conoce como <strong>miembros</strong></p>
+
+<div class="li-note">
+<p>Public y private se usan para controlar el nivel de visibilidad de los miembros de un objeto.</p>
+
+<p>Cuando queremos permitir que estos <strong>"miembros"</strong> puedan ser accedidas desde fuera del objeto, estos se declaran usando la palabra reservada <strong>public</strong> y si por el contrario quisiéramos que estén ocultos y que no puedan ser accedidos desde el exterior del objeto se usa la palabra clave <strong>private</strong>.</p>
+</div>
+
+<p>Para poder acceder a las propiedades o métodos de un objeto desde dentro del mismo objeto se usa una pseudovariable llamada <strong>$this</strong>, la cual es un comodín que hace referencia al propio objeto y sus miembros.</p>
+
+<p>Para poder hacer referencia a cualquiera de los miembros de un objeto se usa el <strong>operador de objeto</strong> <mark>-></mark> (Operador flecha simple) que se construye con un guión medio y el símbolo "mayor que".</p>
+
+<p>Aplicando todo lo anterior vamos a modificar el código del ejemplo anterior añadiendo los mecanismos para poder leer los datos del producto y al final del código vamos a imprimirlos para comprobar que realmente estamos accediendo a la información correcta.</p>
 
 <pre><code>*/
 class Product
@@ -128,12 +142,9 @@ echo "&lt;div&gt;
 
 <p>Hasta aquí pudiera parecer que con clases y objetos estamos haciendo varios pasos extra innecesarios para poder hacer algo que con simples variables o con un arreglo asociativo podríamos haber hecho en menos lineas de código.</p>
 
-<p>Hay que recordar que hacemos todo esto para poder proteger los datos para que no sean modificados, si ponemos atención al código, no hay forma alguna de modificar los valores del producto mientras se ejecuta el programa, además</p>
+<p>Hay que recordar que hacemos todo esto para poder proteger los datos para que no sean modificados, si ponemos atención al código, no hay forma alguna de modificar los valores del producto mientras se ejecuta el programa.</p>
 
 
-<p>Los datos encapsulados en objetos se conocen como propiedades y las funciones encapsuladas en objectos se conocen como métodos.</p>
-
-<p>Al igual que con los datos, también se puede controlar el nivel de acceso desde el exterior a los métodos, permitiendo libre acceso solo a algunas funcionalidades y manteniendo el resto oculto.</p>
 
 <p>Hay dos maneras para crear objetos, una de ellas es la de convertir arreglos en objetos, pero de esta forma no podemos controlar el ocultamiento y el encapsulamiento.</p>
 
