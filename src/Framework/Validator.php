db75dbdee0a99206f2aa1f092e0fe180f9e451f3
Ulises Rendon <fdulises@outlook.com>
2024-09-24 10:29:26 -0600

    [update] creating validator object

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -1,100 +1,195 @@
 <?php
 namespace App\Framework;
+/*
+$test = 5;
+$result = (new Validator($test))
+    ->required()
+    ->match('/\d/', 'not_a_number')
+    ->url('not_a_link')
+    ->custom(fn($value): bool => $value === 3, 'not_a_three')
+    ->min(4)
+    ->max(4)
+    ->equals(4, 'must_be_a_four');
+
+dd($result->isCorrect(), $result->getErrors());
+*/
 class Validator
 {
-    private $items;
-    private $errors = [];
+    protected bool $isCorrect = true;
 
-    public function __construct(array $items = [])
+    protected array $errors = [];
+
+    protected mixed $field;
+
+    public function __construct(mixed $field)
     {
-        $this->items = $items;
-
-        foreach ($items as $field => ['value' => $value, 'conditions' => $conditions]) {
-            $fieldErrors = [];
-            foreach ($conditions as $condition => $argument) {
-                $is_valid = $this->$condition($value, $argument);
-                if (!$is_valid) {
-                    $fieldErrors[$condition] = $condition;
-                }
-            }
-            if (count($fieldErrors)) {
-                $this->errors[$field] = $fieldErrors;
-            }
-        }
+        $this->field = $field;
     }
 
-    public function getErrores(): array
+    public static function field(mixed $field): static
+    {
+        return new static($field);
+    }
+    
+    public function getErrors(): array
     {
         return $this->errors;
     }
-
-    public static function single($value, $condition, $argument = false)
+    
+    public function isCorrect(): bool
     {
-        if (is_string($condition)) {
-            if (!$argument)
-                $argument = $condition;
-            $condition = [$condition => $argument];
+        return $this->isCorrect;
+    }
+
+    public function required(?string $errorName = null): static
+    {
+        if(is_null($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = 'required';
         }
-        $singleValidacion = new self([
-            'single' => [
-                'valor' => $value,
-                'condiciones' => $condition,
-            ]
-        ]);
-        $singleErrores = $singleValidacion->getErrores();
-        if (count($singleErrores))
-            return $singleErrores['single'];
-        return [];
+
+        return $this;
     }
 
-    public function empty($value, $argument): bool
+
+    public function populated(?string $errorName = null): static
     {
-        return empty($value);
+        if(empty($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'populated';
+        }
+
+        return $this;
     }
 
-    public function int($value, $argument): bool
+    public function int(?string $errorName = null): static
     {
-        return is_int($value);
+        if(!is_int($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'int';
+        }
+
+        return $this;
     }
 
-    public function numeric($value, $argument): bool
+    public function bool(?string $errorName = null): static
     {
-        return is_numeric($value);
+        if(!is_bool($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'bool';
+        }
+
+        return $this;
     }
 
-    public function string($value, $argument): bool
+    public function float(?string $errorName = null): static
     {
-        return is_string($value);
+        if(!is_float($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'float';
+        }
+
+        return $this;
     }
 
-    public function max($value, $argument): bool
+    public function numeric(?string $errorName = null): static
     {
-        return $value <= $argument;
+        if(!is_numeric($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = 'numeric';
+            $this->errors[] = $errorName ? $errorName : 'numeric';
+        }
+
+        return $this;
     }
 
-    public function min($value, $argument): bool
+    public function string(?string $errorName = null): static
     {
-        return $value >= $argument;
+        if(!is_string($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'string';
+        }
+
+        return $this;
     }
 
-    public function regex($value, $argument): bool
+    public function max(int|float $value, ?string $errorName = null): static
     {
-        return (bool) preg_match($value, $argument);
+        if($this->field > $value){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'max';
+        }
+
+        return $this;
     }
 
-    public function custom($value, $argument): bool
+    public function min(int|float $value, ?string $errorName = null): static
     {
-        return (bool) $argument($value);
+        if($this->field < $value){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'min';
+        }
+
+        return $this;
     }
 
-    public function email($value, $argument): bool
+    public function equals(mixed $value, ?string $errorName = null): static
     {
-        return (bool) filter_var($value, FILTER_VALIDATE_EMAIL);
+        if($this->field != $value){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'equals';
+        }
+
+        return $this;
+    }
+    public function array(array $value, ?string $errorName = null): static
+    {
+        if(!is_array($value)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'array';
+        }
+
+        return $this;
     }
 
-    public function url($value, $argument): bool
+    public function match(string $value, ?string $errorName = null): static
     {
-        return (bool) filter_var($value, FILTER_VALIDATE_URL);
+        if(!preg_match($value, (string) $this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'match';
+        }
+
+        return $this;
+    }
+
+    public function custom(\Closure $argument, ?string $errorName = null): static
+    {
+        if(!$argument($this->field)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'custom';
+        }
+
+        return $this;
+    }
+
+    public function email(?string $errorName = null): static
+    {
+        if(!filter_var($this->field, FILTER_VALIDATE_EMAIL)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'email';
+        }
+
+        return $this;
+    }
+
+    public function url(?string $errorName = null): static
+    {
+        if(!filter_var($this->field, FILTER_VALIDATE_URL)){
+            $this->isCorrect = false;
+            $this->errors[] = $errorName ? $errorName : 'url';
+        }
+
+        return $this;
     }
 
 }
