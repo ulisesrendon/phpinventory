eb6d02a9a7aa3c9a0089f39e4f5e92a08e1e7995
Ulises Rendon <fdulises@outlook.com>
2024-11-12 16:59:32 -0600

    [wip]

--- src/Database/Migration/Migration.php
+++ src/Database/Migration/Migration.php
@@ -30,26 +30,41 @@ class Migration extends DefaultController
                     ('700000003', 'Mouse Logitech G505 Hero', 1000)
             ");
 
-            $this->DataBaseAccess->command('CREATE table if not exists product_entries(
+            $this->DataBaseAccess->command('CREATE table if not exists entries(
                 id integer not null auto_increment primary key,
-                product_id bigint not null,
-                quantity integer not null,
-                provider_id integer not null,
-                cost decimal(10, 2) not null default 0,
-                lot varchar(255) null,
-                expiration_date timestamp(0) null,
+                folio varchar(255) null,
+                provider_id bigint not null,
+                amount_total decimal(10, 2) not null default 0,
                 deleted_at timestamp(0) null,
                 created_at timestamp(0) null default now(),
                 updated_at timestamp(0) null default now()
             )');
 
-            $this->DataBaseAccess->command('INSERT INTO 
-                product_entries (product_id, quantity, provider_id, cost)
+            $this->DataBaseAccess->command('CREATE table if not exists entrylines(
+                id integer not null auto_increment primary key,
+                entry_id bigint not null,
+                product_id bigint not null,
+                pieces integer not null,
+                cost decimal(10, 2) not null default 0,
+                lot varchar(255) null,
+                expiration_date timestamp(0) null
+            )');
+
+            $this->DataBaseAccess->command(query: 'INSERT INTO 
+                entries (folio, provider_id, amount_total)
                 VALUES 
-                    (1, 10, 1, 800),
-                    (1, 2, 2, 700),
-                    (2, 10, 1, 790),
-                    (3, 15, 3, 650)
+                    (\'1234210\', 1, 800),
+                    (\'123-32421\', 2, 1490),
+                    (\'2H4-28HD2\', 3, 650)
+            ');
+
+            $this->DataBaseAccess->command('INSERT INTO 
+                entrylines (entry_id, product_id, pieces, cost)
+                VALUES 
+                    (1, 1, 10, 800),
+                    (2, 1, 2, 700),
+                    (2, 2, 10, 790),
+                    (3, 3, 15, 650)
             ');
 
             $this->DataBaseAccess->command('CREATE table if not exists product_stocks(
@@ -86,6 +101,71 @@ class Migration extends DefaultController
                     ('Provider #1 el principal'),
                     ('Provider #2 el bueno'),
                     ('Provider #3 el otro')
+            ");
+
+            $this->DataBaseAccess->command('CREATE table if not exists providers(
+                id integer not null auto_increment primary key,
+                title varchar(255) null,
+                description varchar(255) null,
+                deleted_at timestamp(0) null,
+                created_at timestamp(0) null default now(),
+                updated_at timestamp(0) null default now()
+            )');
+
+
+            $this->DataBaseAccess->command('CREATE table if not exists orders(
+                id integer not null auto_increment primary key,
+                customer_id bigint not null,
+                payment_method_id bigint not null,
+                address_id bigint not null,
+                amount_total decimal(10, 2) not null default 0,
+                deleted_at timestamp(0) null,
+                created_at timestamp(0) null default now(),
+                updated_at timestamp(0) null default now()
+            )');
+
+            $this->DataBaseAccess->command('CREATE table if not exists orderlines(
+                id integer not null auto_increment primary key,
+                order_id bigint not null,
+                product_id bigint not null,
+                pieces integer not null,
+                amount_by_piece decimal(10, 2) not null default 0,
+                amount_total decimal(10, 2) not null default 0
+            )');
+
+            $this->DataBaseAccess->command('CREATE table if not exists orderstates(
+                id integer not null auto_increment primary key,
+                state_id bigint not null,
+                created_at timestamp(0) null default now()
+            )');
+
+            $this->DataBaseAccess->command('CREATE table if not exists states(
+                id integer not null auto_increment primary key,
+                name varchar(255) not null unique
+            )');
+
+            $this->DataBaseAccess->command('CREATE table if not exists paymentmethods(
+                id integer not null auto_increment primary key,
+                name varchar(255) not null unique
+            )');
+
+            $this->DataBaseAccess->command("INSERT INTO 
+                states (name)
+                VALUES 
+                    ('Cancelado'),
+                    ('Entregado'),
+                    ('Pagado'),
+                    ('Devuelto'),
+                    ('Intento de entrega'),
+            ");
+
+            $this->DataBaseAccess->command("INSERT INTO 
+                paymentmethods (name)
+                VALUES 
+                    ('Efectivo'),
+                    ('Transferencia bancaria'),
+                    ('Paypal'),
+                    ('Mercadopago')
             ");
 
         } catch (\Exception $e) {
