ab8c1cb8623f5ffa688b1dfd7ad5ab0a89c7e6c3
Ulises Rendon <fdulises@outlook.com>
2024-12-11 15:25:45 -0600

    [update] fix code style

--- content/templates/nav.template.php
+++ content/templates/nav.template.php
@@ -2,6 +2,4 @@
     <?php foreach ($Collection->Contents as $content) { ?>
     <a href="<?php echo $content->url ?>"><?php echo $content->title ?></a>
     <?php } ?>
-    
-    <pre><code><?php print_r($Collection) ?></code></pre>
 </nav>
