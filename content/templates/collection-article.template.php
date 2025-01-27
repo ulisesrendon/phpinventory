4fa3a78a74b337404ce227c018094bb5f5a7ff76
Ulises Rendon <fdulises@outlook.com>
2025-01-27 09:08:06 -0600

    [update] small update to template file

--- content/templates/collection-article.template.php
+++ content/templates/collection-article.template.php
@@ -2,7 +2,7 @@
     <?php foreach ($Collection->Contents as $Content) { ?>
     <div class="card-blogpost bg-white">
         <a href="<?php echo $Content->url ?>">
-            <img width="480" height="270" src="<?php echo $Config['site_url'] ?>/<?php echo $Content->properties->coverSmall ?? 'media/covers/default.480.jpg' ?>"
+            <img width="480" height="270" src="<?php echo $Config->get('site_url') ?>/<?php echo $Content->properties->coverSmall ?? 'media/covers/default.480.jpg' ?>"
                 alt="<?php echo $Content->title ?>" loading="lazy" title="<?php echo $Content->title ?>">
             <div class="blogpost-title"><?php echo $Content->title ?></div>
             <div class="blogpost-descrip"><?php echo $Content->properties->description ?? $Content->title ?></div>
